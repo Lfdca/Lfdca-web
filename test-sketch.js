5666605cbb5fd6fd329f4481fe44f1bed486ded9
Lfdca <77643139+Lfdca@users.noreply.github.com>
2021-02-01 15:12:56 -0300

    very cool 3d effects in a for loop

--- test-sketch.js
+++ test-sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(600,600);
+  createCanvas(400,400);
   print('Hello World');
  console.log('setup');
 
@@ -7,20 +7,14 @@ function setup() {
   // Use a local variable a in for loop
   for (let z=0; z < 221; z += .23) {
     print(z);  
-    line(0, z+1, z+11, height);
- 
-     circle(z+25,130,z);
-    
-  fill(1, 176, 240);
+    line(0, z+1, z*z, height);
+    line(z*z, z+121,z*z,z );
+    circle(z+25,270,z);
+    fill(1, 176, 240);
   
 
 }
 
-  
-
-  
-  fill(1, 176, 240);
-  
   
   
 }
