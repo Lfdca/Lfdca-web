7a647465ffd8d3f672c48d527f8016e0ab24ac5b
Lfdca <77643139+Lfdca@users.noreply.github.com>
2021-02-01 15:31:02 -0300

    done

--- test-sketch.js
+++ test-sketch.js
@@ -1,18 +1,17 @@
 function setup() {
-  createCanvas(400,400);
+  createCanvas(600,600);
   print('Hello World');
  console.log('setup');
 
  //var franc = require('franc');
   // Use a local variable a in for loop
-  for (let z=0; z < 221; z += .23) {
-    print(z);  
-    line(0, z+1, z*z, height);
-    line(z*z, z+121,z*z,z );
-    circle(z+25,270,z);
-    fill(1, 176, 240);
-  
-
+  for (let z=0; z < 221; z += .23){
+    line(z*z*z, z+10, z*z, height);
+    line(z*z*z, 3, z*z, z);
+    line(z+2, z+13, z*z, height);
+    line(z*z*z, z+121,z*z,z );
+    circle(z+50,370,z); 
+    fill(11, 176, 240);
 }
 
   
@@ -20,6 +19,7 @@ function setup() {
 }
 
 function draw() {
+
   
  background(112,03);
 }
