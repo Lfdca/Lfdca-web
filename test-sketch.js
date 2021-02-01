bee1d793114379e6b23c6eb6a54401814517ccd3
Lfdca <77643139+Lfdca@users.noreply.github.com>
2021-02-01 14:42:28 -0300

    Update test-sketch.js

--- test-sketch.js
+++ test-sketch.js
@@ -1,1 +1,29 @@
+function setup() {
+  createCanvas(400, 400);
+  print('Hello World');
+ console.log('aa');
 
+ //var franc = require('franc');
+  // Use a local variable a in for loop
+  for (let z=100; z < 200; z += .3) {
+    print(z);  
+    line(z, 0, z, height);
+     circle(z,20,z);
+
+}
+
+  
+
+  
+  fill(1, 176, 240);
+ ellipse(400, 200, 250, 250);
+  
+  
+  
+  
+}
+
+function draw() {
+  
+ // background(220);*/
+}
